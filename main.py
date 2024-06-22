a5d6defa2f240685faeb30166349e6bcb5a82106
T7HR <theerapat.201031@gmail.com>
2024-06-22 18:45:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,51 +14,56 @@ bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 @bot.event
 async def on_ready():
     print("Bot Online!")
-    print("555")
     synced = await bot.tree.sync()
     print(f"{len(synced)} command(s)")
 
 
-
-
 # แจ้งคนเข้า -ออกเซิฟเวอร์
+
 
 @bot.event
 async def on_member_join(member):
-    channel = bot.get_channel(1140633489520205934) # IDห้อง
-    text = f"Welcome to the server, {member.mention}!"
+    channel = bot.get_channel(1235863937938100247) # IDห้อง
+    text = f"Welcome **{str(member)}** to Server **{member.guild.name}**"
 
     emmbed = discord.Embed(title = 'Welcome to the server!',
                            description = text,
-                           color = 0x66FFFF)
+                           color = 0x0015FF)
 
     await channel.send(text) # ส่งข้อความไปที่ห้องนี้
     await channel.send(embed = emmbed)  # ส่ง Embed ไปที่ห้องนี้
     await member.send(text) # ส่งข้อความไปที่แชทส่วนตัวของ member
+    embed.set_author(name=str(member), icon_url=member.avatar_url)
 
 
 @bot.event
 async def on_member_remove(member):
-    channel = bot.get_channel(1140633489520205934)  # IDห้อง
+    channel = bot.get_channel(1235863937938100247)  # IDห้อง
     text = f"{member.name} has left the server!"
     await channel.send(text)  # ส่งข้อความไปที่ห้องนี้
-
 
 
 # คำสั่ง chatbot
 @bot.event
 async def on_message(message):
     mes = message.content # ดึงข้อความที่ถูกส่งมา
-    if mes == 'hello':
-        await message.channel.send("Hello It's me") # ส่งกลับไปที่ห้องนั่น
+    if mes == 'ฮาโหล':
+        await message.channel.send("โหลควยไร") # ส่งกลับไปที่ห้องนั่น
 
-    elif mes == 'hi bot':
-        await message.channel.send("Hello, " + str(message.author.name))
+    elif mes == '555':
+        await message.channel.send("ยาอ่อน")
 
     await bot.process_commands(message)
     # ทำคำสั่ง event แล้วไปทำคำสั่ง bot command ต่อ
 
 
+@bot.event
+async def on_message(message):
+    mes = message.content # ดึงข้อความที่ถูกส่งมา
+    if mes == 'ฮารุ':
+        await message.channel.send("201031") # ส่งกลับไปที่ห้องนั่น
+
+    await bot.process_commands(message)
 
 
 # ///////////////////// Commands /////////////////////
@@ -70,6 +75,11 @@ async def hello(ctx):
 
 
 @bot.command()
+async def ขอมูล(ctx):
+    await ctx.send(f"hello {ctx.author.name}!")
+
+
+@bot.command()
 async def test(ctx, arg):
     await ctx.send(arg)
 
@@ -77,41 +87,47 @@ async def test(ctx, arg):
 # Slash Commands
 @bot.tree.command(name='hellobot', description='Replies with Hello')
 async def hellocommand(interaction):
-    await interaction.response.send_message("Hello It's me BOT DISCORD")
+    await interaction.response.send_message("555")
 
 
-@bot.tree.command(name='name')
-@app_commands.describe(name = "What's your name?")
+@bot.tree.command(name='พิมพ์ตาม', description='พิมพ์ตามที่เราใส่')
+@app_commands.describe(name = "ใส่ข้อความที่จะให้บอทพิมพ์ตาม")
 async def namecommand(interaction, name : str):
-    await interaction.response.send_message(f"Hello {name}")
+    await interaction.response.send_message(f"{name}")
 
 
 # Embeds
 
-@bot.tree.command(name='help', description='Bot Commands')
+
+@bot.tree.command(name='กฏหมอ', description='กฏหมอทั้งหมด')
+async def test1(interaction):
+    emmbed = discord.Embed(title='Link', url="https://docs.google.com/spreadsheets/d/15Y3MvtGqrZkf9SzLRwJwOk5Z7xGs_ahyx_9Sq8V_CSQ/edit?usp=sharing", description='This is an embed that will show how to build an embed and the different components', color=0x66FFFF, timestamp= discord.utils.utcnow())
+
+    
+    await interaction.response.send_message(embed = emmbed)
+
+
+
+
+@bot.tree.command(name='help', description='กฎหมอทั้งหมด')
 async def helpcommand(interaction):
-    emmbed = discord.Embed(title='Help Me! - Bot Commands',
+    emmbed = discord.Embed(title='Click Link', url='https://docs.google.com/spreadsheets/d/15Y3MvtGqrZkf9SzLRwJwOk5Z7xGs_ahyx_9Sq8V_CSQ/edit?gid=588131081#gid=588131081', 
                            description='Bot Commands',
                            color=0x66FFFF,
                            timestamp= discord.utils.utcnow())
 
 
     # ใส่ข้อมูล
-    emmbed.add_field(name='/hello1', value='Hello Commmand', inline=True)
-    emmbed.add_field(name='/hello2', value='Hello Commmand', inline=True)
-    emmbed.add_field(name='/hello3', value='Hello Commmand', inline=False)
 
-    emmbed.set_author(name='Author', url='https://www.youtube.com/@maoloop01/channels', icon_url='https://yt3.googleusercontent.com/0qFq3tGT6LVyfLtZc-WCXcV9YyEFQ0M9U5W8qDe36j2xBTN34CJ20dZYQHmBz6aXASmttHI=s900-c-k-c0x00ffffff-no-rj')
+    emmbed.set_author(name='Highest', url='https://discord.gg/highest', icon_url='https://media.discordapp.net/attachments/1226563343389556836/1226563460612231228/logo2.png?ex=66779efd&is=66764d7d&hm=9734c98d26eafc1a52ca8b668049ce7eee5bbf73f9391bbf94fc9b7c8b1ccf1b&=&format=webp&quality=lossless')
 
     # ใส่รูปเล็ก-ใหญ่
-    emmbed.set_thumbnail(url='https://yt3.googleusercontent.com/0qFq3tGT6LVyfLtZc-WCXcV9YyEFQ0M9U5W8qDe36j2xBTN34CJ20dZYQHmBz6aXASmttHI=s900-c-k-c0x00ffffff-no-rj')
-    emmbed.set_image(url='https://i.ytimg.com/vi/KZRa9DQzUpQ/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLCfWDgiBYjFJtrUasd5yxmQZJG_cg')
+    emmbed.set_image(url='https://media.discordapp.net/attachments/1226563343389556836/1226979365854904482/500.gif?ex=6677d0d4&is=66767f54&hm=17ad2a5d72dd2a9f4a3a76bddaf7eed074c26037114f75fe310d07b354eacefe&=&width=350&height=350')
 
     # Footer เนื้อหาส่วนท้าย
-    emmbed.set_footer(text='Footer', icon_url='https://yt3.googleusercontent.com/0qFq3tGT6LVyfLtZc-WCXcV9YyEFQ0M9U5W8qDe36j2xBTN34CJ20dZYQHmBz6aXASmttHI=s900-c-k-c0x00ffffff-no-rj')
+    emmbed.set_footer(text='วันที่')
 
     await interaction.response.send_message(embed = emmbed)
-
 
 server_on()
 
